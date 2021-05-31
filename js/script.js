21480bf803caef812636eaa278e66e82d1b689af
Javohir Nurmatjonov <javoxirone@gmail.com>
2021-05-31 19:46:13 +0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+
+
+	$('.slider').slick();
+});
